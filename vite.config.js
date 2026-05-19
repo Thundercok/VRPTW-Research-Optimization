38cfb3511390678eb94735772ade9e8adb0e4bc2
Hài Dón <cathuys69@gmail.com>
2026-05-19 23:45:31 +0700

    chore: setup firebase hosting and github actions

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: resolve(__dirname, 'src/frontend'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/frontend/index.html'),
+        app: resolve(__dirname, 'src/frontend/app.html'),
+        auth: resolve(__dirname, 'src/frontend/auth.html'),
+        feedback: resolve(__dirname, 'src/frontend/feedback.html'),
+        feedback_admin: resolve(__dirname, 'src/frontend/feedback_admin.html')
+      }
+    }
+  }
+});
